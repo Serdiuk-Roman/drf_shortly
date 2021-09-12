6a79465eaff84f77079addaa8d567444f2f12be1
roman <serdiuk.r@gmail.com>
2021-09-12 10:09:19 +0300

    removed token creation from models

--- shortly/shortly_app/models.py
+++ shortly/shortly_app/models.py
@@ -1,10 +1,4 @@
 from django.contrib.auth.models import User
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from rest_framework.authtoken.models import Token
-
 from django.db import models
 
 
@@ -30,9 +24,3 @@ class ShortLinkInfo(models.Model):
     guest = models.CharField(max_length=32, blank=True, null=True)
     last_datetime = models.DateTimeField(auto_now=True)
     click_count = models.PositiveSmallIntegerField(default=0)
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
